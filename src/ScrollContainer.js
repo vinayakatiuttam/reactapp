a72b3ec98cf30511b0e531b1600f4feb16b0d221
vinyakatiuttam <vinayak@glocalise.net>
2023-06-13 15:02:33 +0530

    style adjust

--- src/ScrollContainer.js
+++ src/ScrollContainer.js
@@ -5,6 +5,9 @@ import slide2 from "./slide2.png";
 import slide5 from "./slide5.png";
 import slide6 from "./slide6.png";
 import slide7 from "./slide7.png";
+import slide8 from "./slide8.png";
+import slide3 from "./slide3.png";
+import slide4 from "./slide 4.png";
 
 import Menu from "./Menu.svg";
 
@@ -14,7 +17,7 @@ function ScrollContainer() {
     const scrollContainer = scrollContainerRef.current;
 
     const handleWheel = (event) => {
-      const scrollSpeed = 10; // Adjust the scroll speed as per your preference
+      const scrollSpeed = 25; // Adjust the scroll speed as per your preference
       const delta = Math.sign(event.deltaY); // Detect the direction of the touchpad scroll
       scrollContainer.scrollLeft += delta * scrollSpeed;
     };
@@ -31,14 +34,18 @@ function ScrollContainer() {
         <img src={Menu} alt="" style={{ width: "100%" }} />
         <img src={slide1} alt=""  />
         <img src={slide2} alt="" />
+        <img src={slide3} alt="" />
+        <img src={slide4} alt="" />
       </div>
+    
 
-      <div className="scroll-container" ref={scrollContainerRef}>
-        <div className="scroll-content">
+      <div className="scroll-container" ref={scrollContainerRef} >
+        <div className="scroll-content" >
           {/* <img src={Menu}alt="" style={{width:"100%"}}/> */}
-          <img src={slide5} alt=""  />
-          <img src={slide6} alt=""  />
-          <img src={slide7} alt="" />
+          <img src={slide5} alt=""  style={{height:"100%",width:"100%", }}/>
+          <img src={slide6} alt=""  style={{height:"100%",width:"100%"}}/>
+          <img src={slide7} alt="" style={{height:"100%",width:"100%"}}/>
+          <img src={slide8} alt="" style={{height:"100%",width:"100%"}}/>
         </div>
       </div>
     </div>
